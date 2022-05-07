108d1cd02348848a0f5bc937e05d8c29ebe557e5
macs1756 <kindrativmaksum312@gmail.com>
2022-05-07 12:36:17 +0200

    num1

--- js/main.js
+++ js/main.js
@@ -7,4 +7,14 @@ const burger = document.querySelector("#burger_active");
 button.addEventListener("click", function(){
 			burger.classList.toggle("trans");
 });
-const tt = "gg";
+/////////////////
+
+
+const click = document.querySelector("#click_btn"); 
+
+const content = document.querySelector("#click__content"); 
+console.log(content);
+
+click.addEventListener("click", function(){
+			content.classList.toggle("hidden");
+})
